1fbc087eb4509838bae31e083d7ba6fb169251ba
arnima <arnima@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-11-09 23:43:36 +0000

    Enabled LazyData and improved help pages.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/hafroassmt@16 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/zlines.R
+++ pkg/R/zlines.R
@@ -1,15 +1,14 @@
-zlines <-
-function(slope, by = 1, ...)
+zlines <- function(slope, by=1, ...)
 {
-  if (slope == 0)
-    stop("lines are supposed to be oblique, use 'grid' or 'abline' instead")
-  if (slope < 0)
-    intercepts <- seq(floor(par("usr")[3] - slope*par("usr")[1]),
-      ceiling(par("usr")[4] - slope*par("usr")[2]), by = by)
+  if(slope == 0)
+    stop("lines are supposed to be oblique, use grid() or abline() instead")
+  if(slope < 0)
+    intercepts <- seq(floor(par("usr")[3]-slope*par("usr")[1]), ceiling(par("usr")[4]-slope*par("usr")[2]), by=by)
   else
-    intercepts <- seq(ceiling(par("usr")[4] - slope*par("usr")[1]),
-      floor(par("usr")[3] - slope*par("usr")[2]), by = -by)
-  for(i in seq(along = intercepts))
-    abline(a = intercepts[i], b = slope, ...)
-}
+    intercepts <- seq(ceiling(par("usr")[4]-slope*par("usr")[1]), floor(par("usr")[3]-slope*par("usr")[2]), by=-by)
 
+  for(i in seq(along=intercepts))
+    abline(a=intercepts[i], b=slope, ...)
+
+  invisible(NULL)
+}
