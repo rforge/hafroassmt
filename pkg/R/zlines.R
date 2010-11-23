8645eec2002ce5738b385b9ba903cd874fb96754
arnima <arnima@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-11-23 09:46:23 +0000

    zlines() returns intercepts instead of NULL
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/hafroassmt@24 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/zlines.R
+++ pkg/R/zlines.R
@@ -1,14 +1,13 @@
 zlines <- function(slope, by=1, ...)
 {
   if(slope == 0)
-    stop("lines are supposed to be oblique, use grid() or abline() instead")
-  if(slope < 0)
+    stop("Lines are supposed to be oblique, use grid() or abline() instead")
+  else if(slope < 0)
     intercepts <- seq(floor(par("usr")[3]-slope*par("usr")[1]), ceiling(par("usr")[4]-slope*par("usr")[2]), by=by)
   else
     intercepts <- seq(ceiling(par("usr")[4]-slope*par("usr")[1]), floor(par("usr")[3]-slope*par("usr")[2]), by=-by)
 
-  for(i in seq(along=intercepts))
-    abline(a=intercepts[i], b=slope, ...)
+  sapply(intercepts, abline(b=slope, ...))
 
-  invisible(NULL)
+  invisible(intercepts)
 }
