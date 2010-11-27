764fc636f7c246af1ee8a931a563061671ff182b
arnima <arnima@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-11-27 19:28:50 +0000

    Fixed sapply bug in zlines
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/hafroassmt@26 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/zlines.R
+++ pkg/R/zlines.R
@@ -7,7 +7,7 @@ zlines <- function(slope, by=1, ...)
   else
     intercepts <- seq(ceiling(par("usr")[4]-slope*par("usr")[1]), floor(par("usr")[3]-slope*par("usr")[2]), by=-by)
 
-  sapply(intercepts, abline(b=slope, ...))
+  sapply(intercepts, abline, b=slope, ...)
 
   invisible(intercepts)
 }
