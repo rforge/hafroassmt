2efbce29e93dd676200c6ac26d8c4ff946a0a6fb
arnima <arnima@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-05-24 20:23:43 +0000

    Added argument 'div' to surveyIndex()
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/hafroassmt@34 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/surveyIndex.R
+++ pkg/R/surveyIndex.R
@@ -1,12 +1,16 @@
-surveyIndex <- function(tegund, lengd=NULL, sur="smb", data=aggrVisit)
+function(tegund, lengd=NULL, sur="smb", div=1, data=aggrVisit)
 {
-  output <- data[data$tegund==tegund & data$svaedi=="Heild" & data$sur==sur,]
+  ## 1  Filter species and survey, select total area
+  output <- data[data$tegund==tegund & data$sur==sur & data$svaedi=="Heild",]
 
+  ## 2  Filter size group, select columns
   if(is.null(lengd))
     lengd <- min(output$lengd)
   output <- output[output$lengd==lengd,]
   output <- output[c("ar","bio.staerri","cv.bio.staerri","svaedi","svaedisnr","lengd","tegund","sur")]
 
+  ## 3  Scale values, rename rows
+  output$bio.staerri <- output$bio.staerri / div
   row.names(output) <- seq_along(row.names(output))
 
   return(output)
