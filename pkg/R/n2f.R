f3f819fd77039016ac5f832a0622053867ede8bd
arnima <arnima@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-02-24 01:45:20 +0000

    Add function shepherd(), rename popes.approx() to pope()
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/hafroassmt@41 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/n2f.R
+++ pkg/R/n2f.R
@@ -1,10 +1,13 @@
-n2f <-
-function(natage,natMor=0.2) {
-  fatage <- log(natage[-nrow(natage), -ncol(natage)]/natage[-1, -1]) - natMor
-  if(!is.null(dimnames(natage))) {
-    yearAgeRanges <- dim(natage)
-    dimnames(fatage)[[1]] <- dimnames(natage)[[1]][-yearAgeRanges[1]]
-    dimnames(fatage)[[2]] <- dimnames(natage)[[2]][-yearAgeRanges[2]]
+n2f <- function(Nmat, M=0.2)
+{
+  Fmat <- log(Nmat[-nrow(Nmat),-ncol(Nmat)] / Nmat[-1,-1]) - M
+
+  if(!is.null(dimnames(Nmat)))
+  {
+    yearAgeRanges <- dim(Nmat)
+    dimnames(Fmat)[[1]] <- dimnames(Nmat)[[1]][-yearAgeRanges[1]]
+    dimnames(Fmat)[[2]] <- dimnames(Nmat)[[2]][-yearAgeRanges[2]]
   }
-  fatage
+
+  return(Fmat)
 }
